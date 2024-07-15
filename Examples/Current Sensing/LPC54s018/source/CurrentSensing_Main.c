2a02515c761306c43875f61356efe247034aaf1e
Paolo Falduto <paolo.falduto@nxp.com>
2024-07-15 12:00:46 +0200

    Update License as amit suggestion

--- Examples/Current Sensing/LPC54s018/source/CurrentSensing_Main.c
+++ Examples/Current Sensing/LPC54s018/source/CurrentSensing_Main.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
- * Copyright 2016-2017, 2023 NXP
+ * Copyright 2016-2017, 2023-2024 NXP
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
